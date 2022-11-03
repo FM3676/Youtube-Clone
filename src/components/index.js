0c1223d5be8f1c7cef43aa47a9ca79e630bde77e
§ <1309346392@qq.com>
2022-11-03 19:30:16 +0800

    NavBar

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ export { default as Feed } from "./Feed/Feed.component";
 export { default as ChannelDetail } from "./ChannelDetail/ChannelDetail.component";
 export { default as SearchFeed } from "./SearchFeed/SearchFeed.component";
 export { default as VideoDetail } from "./VideoDetail/VideoDetail.component";
-
+export { default as SearchBar } from "./SearchBar/SearchBar.component";
